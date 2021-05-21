197e658022a4bc3d5c03324e3d4278c2113a0433
utkalikajena <utkalikajena97@gmail.com>
2021-05-21 20:17:05 +0530

    CALCULATED: length of line1

--- src/main/java/bridgelabz30/LineComparison.java
+++ src/main/java/bridgelabz30/LineComparison.java
@@ -2,4 +2,10 @@ package bridgelabz30;
 
 public class LineComparison {
 
+    public double lengthOfLine1( int x1 , int x2 , int y1 , int y2 ){
+        double lengthLine1 ;
+        lengthLine1 = Math.sqrt((x2 - x1)^2 + (y2 - y1)^2);
+        return lengthLine1 ;
+    }
+
 }
--- src/main/java/bridgelabz30/Main.java
+++ src/main/java/bridgelabz30/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String args[]){
         LineComparison lineComparison = new LineComparison();
 
-        System.out.println("Welcome to line comparison computation problem");
+        System.out.println("The length og line1 is: " +lineComparison.lengthOfLine1(2,3,4,6));
+
     }
 }
