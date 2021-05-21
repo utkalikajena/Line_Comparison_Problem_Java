60bf3cfcadd52b705c2608d087bbb5ba68648f9d
utkalikajena <utkalikajena97@gmail.com>
2021-05-21 19:42:22 +0530

    ADDED: Greeting messege

---
+++ src/main/java/bridgelabz30/LineComparison.java
@@ -0,0 +1,5 @@
+package bridgelabz30;
+
+public class LineComparison {
+
+}
---
+++ src/main/java/bridgelabz30/Main.java
@@ -0,0 +1,9 @@
+package bridgelabz30;
+
+public class Main {
+    public static void main(String args[]){
+        LineComparison lineComparison = new LineComparison();
+
+        System.out.println("Welcome to line comparison computation problem");
+    }
+}
